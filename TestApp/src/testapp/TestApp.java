1f520ba783157d6aafc1a6cf489c39738e1e22c9
Jakob <Jakobpaul13@gmail.com>
2018-05-21 10:39:06 +1000

    Second Commit

--- TestApp/src/testapp/TestApp.java
+++ TestApp/src/testapp/TestApp.java
@@ -18,6 +18,7 @@ public class TestApp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("test");
+        System.out.println("Hello my name is Jfarr");
     }
     
 }
